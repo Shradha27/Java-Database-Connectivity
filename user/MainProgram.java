d3f2b38043ef865f7f83e0737fdea0c1efb71019
Shradha27 <shradha2704@gmail.com>
2018-02-03 23:36:25 +0530

    Main program

---
+++ user/MainProgram.java
@@ -0,0 +1,11 @@
+package user;
+public class MainProgram {
+
+	public static void main(String[] args) {
+		
+       String username="Shradha", password="qwerty";
+       System.out.println("Username: "+username+"\nPassword: "+password);
+
+	}
+
+}
