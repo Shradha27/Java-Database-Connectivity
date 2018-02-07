cef785801e7517686e93d2beb0ace2fc86065075
Shradha27 <shradha2704@gmail.com>
2018-02-07 22:07:38 +0530

    Input-output functions

---
+++ user/Input.java
@@ -0,0 +1,18 @@
+package user;
+import java.util.Scanner;
+
+public class Input {
+    GetterSetter gs=new GetterSetter();
+	void returninput()
+	{
+	   Scanner sc=new Scanner(System.in);
+	   System.out.println("Enter username");
+	   gs.setUsername(sc.next());
+	   System.out.println("Enter password");
+	   gs.setPassword(sc.next());
+	}
+	void returnoutput()
+	{
+		System.out.println("Username: "+gs.getUsername()+"\nPassword: "+gs.getPassword());
+	}
+}
--- user/MainProgram.java
+++ user/MainProgram.java
@@ -3,9 +3,8 @@ package user;
 public class MainProgram {
 
 	public static void main(String[] args) {
-		GetterSetter gs = new GetterSetter();
-		gs.setUsername("Shradha");
-		gs.setPassword("password");
-		System.out.println("Username: "+gs.getUsername()+"\nPassword: "+gs.getPassword());
+		Input i=new Input();
+		i.returninput();
+		i.returnoutput();
 	}
 }
